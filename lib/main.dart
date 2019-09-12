51baf900633f3db46346de725f4f2bf3f0d8b6cd
Jamie Gardner <jamie.gardner@civica.co.uk>
2019-09-12 13:59:12 +0100

    Add title, banner and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static final Color background = Color(0xffcbcbcb);
+  static final Color backgroundDark = Color(0xff7e8082);
+  static final Color primary = Color(0xff41938E);
+  static final Color light = Color(0xfffefefe);
+  static final Color iconDark = Color(0xff252b2f);
+  static final Color iconMid = Color(0xffa0a0a0);
+  static final Color text = Color(0xff1d1f23);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(fontFamily: 'Roboto'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +32,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  /*
+   * Colour scheme
+   * background: #CBCBCB
+   * background_dark: #7E8082
+   * primary: #41938E
+   * light: #FEFEFE
+   * icon_dark: #252B2F
+   * icon_mid: #A0A0A0
+  */
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    Widget _topBanner = Container(
+      color: MyApp.backgroundDark,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              "Important Payment information - Find out more",
+              style: TextStyle(
+                color: MyApp.light,
+                fontSize: 14.0,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Icon(
+              FontAwesomeIcons.angleRight,
+              color: MyApp.light,
+              size: 18.0,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+    Widget _heading = Padding(
+      padding: EdgeInsets.fromLTRB(15.0,20.0,15.0,10.0),
+      child: Text(
+        "Translink bus and rail tickets direct to your mobile phone",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: MyApp.text,
+          fontSize: 25.0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyApp.background,
+        title: Image.asset(
+          "assets/mlink_title.png",
+          width: 90.0,
+        ),
+        elevation: 0.0,
+      ),
+      backgroundColor: MyApp.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _topBanner,
+            _heading,
+          ],
+        ),
+      ),
     );
   }
 }
